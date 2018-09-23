afa1c0fa123aeff08cbb2fbcf9ff83ac7988bc64
Dromediansk <pillar.mr@gmail.com>
2018-09-23 11:19:02 +0200

    added button for toggle details

--- src/People.js
+++ src/People.js
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './People.css';
 
-const People = ({ key, id, name, height, mass, hair_color, skin_color, eye_color, birth_year, gender }) => {
-	return (
-		<div className='people card'>
-			<h2> { name } </h2>
-            <p> height: {height} </p>
-            <p> Mass: {mass} </p>
-            <p> Hair Color: {hair_color} </p>
-            <p> Skin Color: {skin_color} </p>
-            <p> Eye Color: {eye_color} </p>
-            <p> Birth Year: {birth_year} </p>
-            <p> Gender: {gender} </p>
-		</div>
+export class People extends Component {
+	constructor() {
+		super();
+		this.state = {
+      	showDetails: false,
+    }
+    this.showDetails = this.showDetails.bind(this);
+    this.hideDetails = this.hideDetails.bind(this);
+  }
+
+  showDetails(event) {
+  	event.preventDefault();
+  	this.setState({
+  		showDetails: true
+  	});
+  }
+
+  hideDetails() {
+  	this.setState({
+  		showDetails: false
+  	});
+  }
+
+  render() {
+		return (
+	<div className='people card'>
+		<h2> {this.props.name} </h2>
+		{
+		this.state.showDetails
+		? (
+			<div>
+				<div onClick={this.hideDetails} id='hide'>
+					<i className='fa fa-chevron-circle-up fa-5px'></i>
+				</div>
+				<ul className='details'>
+		            <li> Height: {this.props.height}</li>
+		            <li> Mass: {this.props.mass}</li>
+		            <li> Hair Color: {this.props.hair_color}</li>
+		            <li> Skin Color: {this.props.skin_color}</li>
+		            <li> Eye Color: {this.props.eye_color}</li>
+		            <li> Birth Year: {this.props.birth_year}</li>
+		            <li> Gender: {this.props.gender}</li>
+	        	</ul>
+			</div>
+
+        	)
+		: (
+			<div onClick={this.showDetails} className='show'>
+				<i className='fa fa-chevron-circle-down fa-5px'></i>
+			</div>
+			)
+        }
+	</div>
 	);
+  }
 }
 
 export default People;
